c8639b8fba264f7ba2d3b7289351d5f7b8cfcac2
Sean Saulnier <sean@seansaulnier.com>
2013-12-24 10:28:57 -0500

    Removed debug in authentication

--- src/PurpleNeve/ControlPanelBundle/Security/User/WebserviceUserProvider.php
+++ src/PurpleNeve/ControlPanelBundle/Security/User/WebserviceUserProvider.php
@@ -17,7 +17,6 @@ class WebserviceUserProvider implements UserProviderInterface {
     }
     
     public function loadUserByUsername($username) {
-        debugbreak('666@209.29.22.251:7869;d=1,p=0,c=0');
         $userObj = new \PurpleNeve\ControlPanelBundle\Controller\UserController();
         $userObj->setContainer($this->container);
         
